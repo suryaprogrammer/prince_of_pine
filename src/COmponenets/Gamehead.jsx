835e9ff602fb20ed66d2d81905bc2bb30f4d0cae
surya sundar <codewithsurya7@gmail.com>
2021-11-14 14:46:47 +0530

    the prince of pine

---
+++ src/COmponenets/Gamehead.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Gamehead = () => {
+    return (
+        <>
+         <div id="topic"><h1>The Prince of Pine</h1></div>   
+        </>
+    )
+}
+
+export default Gamehead
